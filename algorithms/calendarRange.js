046c4687e0202b7098360afb1a817b9432e0faf0
Paolo Razon <plgrazon@gmail.com>
2018-10-15 12:55:44 -0700

    [ops] - Add solution to calendar range algorithm.

--- algorithms/calendarRange.js
+++ algorithms/calendarRange.js
@@ -7,15 +7,41 @@ const calendarRange = (meetingSchedules) => {
   });
 
   // initalize result array with the earliest meeting
-  return mergedMeetings = [sortedMeetings[0]];
+  const mergedMeetings = [sortedMeetings[0]];
+
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    let currentMeeting = sortedMeetings[i];
+    let lastMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+    if (currentMeeting.startTime <= lastMeeting.endTime) {
+      lastMeeting.endTime = Math.max(currentMeeting.endTime, lastMeeting.endTime);
+    } else {
+      mergedMeetings.push(currentMeeting);
+    }
+  }
+
+  return mergedMeetings;
 }
 
 /*
+input:
 [
-{ startTime: 0,  endTime: 1 },
-{ startTime: 3,  endTime: 5 },
-{ startTime: 4,  endTime: 8 },
-{ startTime: 10, endTime: 12 },
-{ startTime: 9,  endTime: 10 },
+  { startTime: 0,  endTime: 1 },
+  { startTime: 3,  endTime: 5 },
+  { startTime: 4,  endTime: 8 },
+  { startTime: 9,  endTime: 10 },
+  { startTime: 10, endTime: 12 }
+]
+merged:
+[
+  { st: 0, et: 1},
+  { st: 3, et: 8},
+  { st: 9, et: 12}
+]
+output:
+[
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 8 },
+  { startTime: 9, endTime: 12 },
 ]
 */

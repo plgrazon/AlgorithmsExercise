bfd4177f017d27b1dd75df83d886a1b3e1a94a25
Paolo Razon <plgrazon@gmail.com>
2018-10-15 12:21:58 -0700

    [ops] - Initalize calendarRange problem.

--- algorithms/calendarRange.js
+++ algorithms/calendarRange.js
@@ -1,10 +1,21 @@
 const calendarRange = (meetingSchedules) => {
   // create a deep copy
-  const meetingCopy = JSON.parse(JSON.stringify(meetingSchedules));
+  const meetingsCopy = JSON.parse(JSON.stringify(meetingSchedules));
 
-  const sortedMeetings = meetingsCopy.sort(a, b) => {
+  const sortedMeetings = meetingsCopy.sort((a, b) => {
     return a.startTime - b.startTime;
   });
 
-  return sortedMeetings;
+  // initalize result array with the earliest meeting
+  return mergedMeetings = [sortedMeetings[0]];
 }
+
+/*
+[
+{ startTime: 0,  endTime: 1 },
+{ startTime: 3,  endTime: 5 },
+{ startTime: 4,  endTime: 8 },
+{ startTime: 10, endTime: 12 },
+{ startTime: 9,  endTime: 10 },
+]
+*/

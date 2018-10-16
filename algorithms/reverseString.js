36b5e392e026cb76617dbe7e9651885936233af5
Paolo Razon <plgrazon@gmail.com>
2018-10-15 17:24:51 -0700

    [ops] - Add reverse string algorithm.

---
+++ algorithms/reverseString.js
@@ -0,0 +1,14 @@
+const reverseString = (str) => {
+  const input = str.split('');
+  let leftIndex = 0;
+  let rightIndex = str.length - 1;
+
+  while(leftIndex < rightIndex) {
+    let temp = input[leftIndex];
+    input[leftIndex] = input[rightIndex];
+    input[rightIndex] = temp;
+    leftIndex++;
+    rightIndex--;
+  }
+  return input.join('');
+}

a263ea11e1945027045d13a6f4ab37398c4ef10d
Paolo Razon <plgrazon@gmail.com>
2018-10-31 10:58:56 -0700

    [ops] - Add twoSums algorithm.

---
+++ algorithms/twoSum.js
@@ -0,0 +1,21 @@
+/**
+  Given and array of numbers return the index
+  of the two nums that will equate to the target
+
+  input: [1,2,3,4,5], 3
+  output: [0,1]
+**/
+
+const twoSums = (nums, target) => {
+  const remainders = {};
+  for (let i = 0; i < nums.length; i++) {
+    let curr = nums[i];
+    let rem = target - curr;
+    if (rem in remainders) {
+      return [remainders[rem], i];
+    }
+    remainders[curr] = i;
+  }
+}
+
+console.log(twoSums([2,7,11,15], 9));

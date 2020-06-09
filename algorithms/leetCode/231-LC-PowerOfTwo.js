88dd5439884d68cd94c1a3ef1a843414659b0b17
plgrazon <plgrazon@gmail.com>
2020-06-08 22:02:28 -0400

    leetcode - Add daily problem

---
+++ algorithms/leetCode/231-LC-PowerOfTwo.js
@@ -0,0 +1,28 @@
+/**
+ * Given an integer, write a function to determine if it is a power of two.
+ *
+ * Example 1:
+ * Input: 1
+ * Output: true
+ * Explanation: 2^0 = 1
+ *
+ * Example 2:
+ * Input: 16
+ * Output: true
+ * Explanation: 2^4 = 16
+ *
+ * Example 3:
+ * Input: 218
+ * Output: false
+ */
+const isPowerOfTwo = function (n) {
+  if (!n) return false;
+  while (n !== 1) {
+    if (n % 2 !== 0) return false;
+    n = n / 2;
+  }
+
+  return true;
+};
+
+const isPowerOfTwoOneLiner = (n) => Math.log2(n) % 1 === 0;

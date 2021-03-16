e765f905fa356f28121c652241a392717bf959ba
plgrazon <plgrazon@gmail.com>
2021-03-16 11:25:53 -0400

    dp - Add optimized climbing stairs nth step

--- algorithms/dynamicProgramming/climbingStairsNsteps.js
+++ algorithms/dynamicProgramming/climbingStairsNsteps.js
@@ -15,7 +15,7 @@
  * 5. Where to look for the answer
  *   f(n)
  */
-// Time: O(n);
+// Time: O(nk) => O(n);
 // Space: O(n);
 function climbStairsNwaysArray(n, k) {
   let hash = Array(n + 1).fill(0);
@@ -31,12 +31,38 @@ function climbStairsNwaysArray(n, k) {
   return hash[n];
 }
 
+// Time: O(nk) => O(n);
+// Space: O(k);
+function climbStairsNwaysArrayOptimized(n, k) {
+  // instead of storing n elements we can just store the last k nums.
+  let hash = Array(k).fill(0);
+  hash[0] = 1;
+  hash[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    // we don't need j <= k because we access the 0 element
+    // we can just use j < k
+    for (let j = 1; j < k; j++) {
+      if (i - j < 0) continue;
+      hash[i % k] += hash[(i - j) % k];
+    }
+  }
+
+  return hash[n % k];
+}
+
 let example1 = climbStairsNwaysArray(5, 2); // 8
 let example2 = climbStairsNwaysArray(4, 3); // 7
 let example3 = climbStairsNwaysArray(3, 2); // 3
 let example4 = climbStairsNwaysArray(5, 3); // 13
 
+let example5 = climbStairsNwaysArrayOptimized(5, 2); // 8
+let example6 = climbStairsNwaysArrayOptimized(5, 3); // 13
+
 console.log(example1);
 console.log(example2);
 console.log(example3);
 console.log(example4);
+
+console.log('optimized', example5);
+console.log('optimized', example6);

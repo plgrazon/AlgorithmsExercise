0d29a23f20a8818e64a040bdb36c33e13ae25824
plgrazon <plgrazon@gmail.com>
2021-03-16 14:48:47 -0400

    algo - Add new problem

---
+++ algorithms/arrayAlgorithms/getProductExceptIndex.js
@@ -0,0 +1,22 @@
+/**
+ * This problem was asked by Uber.
+ *
+ * Given an array of integers, return a new array such that each element at
+ * index i of the new array is the product of all the numbers in the original
+ * array except the one at i.
+ *
+ * For example, if our input was [1, 2, 3, 4, 5], the expected output
+ * would be [120, 60, 40, 30, 24]. If our input was
+ */
+// Time: O
+// Space: O
+function getProductExceptIndex(arr) {
+  let total = arr.reduce((acc, num) => acc * num);
+  return arr.map((num) => total / num);
+}
+
+const example1 = getProductExceptIndex([1, 2, 3, 4, 5]);
+const example2 = getProductExceptIndex([3, 2, 1]);
+
+console.log(example1);
+console.log(example2);
